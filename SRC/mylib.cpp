f11e21793a1a07d7a2449e9da0ff5d01a796f4a5
Nocode <arhcnocode@gmail.com>
2025-03-02 09:29:10 +0300

    Update methods

--- SRC/mylib.cpp
+++ SRC/mylib.cpp
@@ -8,63 +8,7 @@ string get_filepath(){
     return "/home/" + username + "/.todoconfig";
 }
 
-int add(string note, bool rewrite) {
-
-    string filePath = get_filepath();
-    ofstream wf;
-
-    if (rewrite == true)
-        wf.open(filePath); 
-    else
-        wf.open(filePath, ios::app);
-
-    if (!wf.is_open()) {
-        cerr << "Ошибка открытия файла для записи!" << endl;
-        return -1;
-    }
-
-    wf << note << endl;
-    wf.close();
-    return 0;
-}
-
-int remove(int id) {
-    id--;
-    string filePath = get_filepath();
-    ifstream rf(filePath);
-    if (!rf.is_open()) {
-        cerr << "Ошибка открытия файла для чтения!" << endl;
-        return -1;
-    }
-
-    vector<string> lines;
-    string line;
-    while (getline(rf, line)) {
-        lines.push_back(line);
-    }
-    rf.close();
-
-    if (id < 0 || id >= lines.size()) {
-        cerr << "Некорректный ID заметки!" << endl;
-        return -1;
-    }
-
-    ofstream wf(filePath);
-    if (!wf.is_open()) {
-        cerr << "Ошибка открытия файла для записи!" << endl;
-        return -1;
-    }
-
-    for (size_t i = 0; i < lines.size(); ++i) {
-        if (i != id) {
-            wf << lines[i] << endl;
-        }
-    }
-    wf.close();
-    return 0;
-}
-
-int edit(string new_note, int id) {
+int rewrite_note(int id, string new_note){
     id--;
     string filePath = get_filepath();
     ifstream rf(filePath);
@@ -96,12 +40,45 @@ int edit(string new_note, int id) {
             wf << lines[i] << endl;
 
         else
-            wf << new_note << endl;
+            wf << new_note;
     }
     wf.close();
     return 0;
 }
 
+int add(string note, bool rewrite) {
+
+    string filePath = get_filepath();
+    ofstream wf;
+
+    if (rewrite == true)
+        wf.open(filePath); 
+    else
+        wf.open(filePath, ios::app);
+
+    if (!wf.is_open()) {
+        cerr << "Ошибка открытия файла для записи!" << endl;
+        return -1;
+    }
+
+    wf << note << endl;
+    wf.close();
+    return 0;
+}
+
+int remove(int id) {
+    if (rewrite_note(id, "") != 0){
+        return -1;
+    }
+    return 0;
+}
+
+int edit(string new_note, int id) {
+    if (rewrite_note(id, new_note + "\n") != 0){
+        return -1;
+    }
+    return 0;
+}
 
 int list(){
     string filePath = get_filepath();
